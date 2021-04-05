5fdbb0b9a4f65d781f90fdfc63f4f5f5d2cc3f4f
Alex Evgrashin <evgrashin.adl@gmail.com>
2021-04-05 19:41:30 +0300

    Initial Commit

---
+++ Shared/DemoDay_RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDay_RandomApp.swift
+//  Shared
+//
+//  Created by labsallday labsallday on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoDay_RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
