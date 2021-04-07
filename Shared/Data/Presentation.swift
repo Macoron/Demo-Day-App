a07c01a9f30a9406a815eee06d479deba0e1b7f1
Alex Evgrashin <evgrashin.adl@gmail.com>
2021-04-07 13:42:54 +0300

    Working on presentations database

--- Shared/Data/Presentation.swift
+++ Shared/Data/Presentation.swift
@@ -1,23 +1,43 @@
 import Foundation
 
-class Presentation : Identifiable, ObservableObject {
-    var id = UUID()
+class Presentation : Identifiable, ObservableObject, Codable {
+    // avg time in seconds for one person to finish presentation
+    static let timePerCodable = 60.0 * 4
     
+    var id = UUID()
     @Published var name : String
     @Published var speakersCount = 1
+    
+    var assumedTime : TimeInterval {
+        return Double(speakersCount) * Presentation.timePerCodable
+    }
     
     init(name: String, speakersCount: Int = 1) {
         self.name = name
         self.speakersCount = speakersCount
     }
     
-    // avg time in seconds for one person to finish presentation
-    static let timePerMember = 60.0 * 4
+    enum CodingKeys: CodingKey {
+        case id, name, speakersCount
+    }
     
-    var assumedTime : TimeInterval {
-        return Double(speakersCount) * Presentation.timePerMember
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        speakersCount = try container.decode(Int.self, forKey: .speakersCount)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(speakersCount, forKey: .speakersCount)
     }
 }
+
 
 func calcTotalTime(_ presentations: [Presentation]) -> TimeInterval {
     return presentations.reduce(0, { res, p in
@@ -32,6 +52,42 @@ func createShareReport(_ presentations: [Presentation]) -> String {
     }
     
     return ret
+}
+
+func getDataPath() -> URL {
+    let appDirectory = NSSearchPathForDirectoriesInDomains(.applicationSupportDirectory,
+                                                                .userDomainMask, true).first!
+    
+    let dataPath = URL(fileURLWithPath: appDirectory).appendingPathComponent("data.json")
+    return dataPath
+}
+
+func loadData() -> [Presentation] {
+    let dataPath = getDataPath()
+    
+    do {
+        let json = try String(contentsOf: dataPath).data(using: .utf8)!
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode([Presentation].self, from: json)
+        
+    } catch {
+        print("Data file not found or invalid (might be first application start)")
+        return []
+    }
+}
+
+func saveData(_ presentations: [Presentation]) {
+    let dataPath = getDataPath()
+    
+    do {
+        let encoder = JSONEncoder()
+        let json = try encoder.encode(presentations)
+        
+        try json.write(to: dataPath)
+    } catch {
+        print("Failed to save data!")
+    }
 }
 
 let testProjects = [
